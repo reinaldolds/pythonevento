31afa512a22d803de426c2bf91b0d90b875e80f5
reinaldolds <reinaldolira8@gmail.com>
2024-09-23 12:29:18 -0300

    Exercicio 3

---
+++ exercico3.py
@@ -0,0 +1,10 @@
+''': int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);'''
+
+indice = 12
+soma = 0 
+k = 1
+while k < indice:
+    k = k + 1
+    soma = soma + k
+
+print(soma)
